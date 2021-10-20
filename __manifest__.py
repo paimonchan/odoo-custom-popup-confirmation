b15d77fd0fb89c634fe73c358e77caa77e198f35
Paimon <nazzunhanif@gmail.com>
2021-10-21 06:55:14 +0700

    initial module

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Custom Popup Confirmation",
+    "summary": "Show custom popup that can be called from backend code",
+    "version": "15.0.0",
+    "category": "Other",
+    "author": "Nazzun Hanif A.",
+    "website": "https://github.com/paimonchan/odoo-stock-picking-cancelation",
+    "depends": [],
+    "data": [
+    ],
+    "installable": True,
+}
