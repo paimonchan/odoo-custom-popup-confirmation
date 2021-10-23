f4f0ad1ce8a32c858fc0bba3defd9e13a7aae31a
Paimon <nazzunhanif@gmail.com>
2021-10-23 19:02:14 +0700

    fix wrong name model access

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
     "website": "https://github.com/paimonchan/odoo-stock-picking-cancelation",
     "depends": [],
     "data": [
-        'security/ir.mode.access.csv',
+        'security/ir.model.access.csv',
         'views/custom_popup_confirmation.xml'
     ],
     "installable": True,
