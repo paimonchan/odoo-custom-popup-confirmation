4f707bf4674dcf548cddb8b8d49d64a702a8df24
Paimon <nazzunhanif@gmail.com>
2021-10-23 19:03:05 +0700

    adjust log message change into warning type and refactor some code

--- models/custom_popup_confirmation.py
+++ models/custom_popup_confirmation.py
@@ -8,26 +8,26 @@ _logger = logging.getLogger(__name__)
 # TODO: add example how to used this custom popup
 class CustomPopupConfirmation(models.TransientModel):
     _name = 'paimon.custom.popup.confirmation'
+    _description = "Custom Popup Confirmation"
 
     message = fields.Char()
     callback = fields.Char()
     source_model = fields.Char()
 
-    def show(self, records, message, callback_name):
+    def show(self, records, message, callback_name=None):
         context = dict(self.env.context)
         context.update(record_ids=records.ids)
 
         popup = self.create(dict(
             message=message,
             callback=callback_name,
-            source_model=records._model,)
+            source_model=records._name,)
         )
 
         action =  dict(
             target = 'new',
             res_id=popup.id,
             view_mode = 'form',
-            name = _(self.title),
             res_model = self._name,
             type = 'ir.actions.act_window',
             context = context,
@@ -41,15 +41,15 @@ class CustomPopupConfirmation(models.TransientModel):
         # check if model is exist
         if not model:
             message = 'not found model {}'.format(self.source_model)
-            _logger.error(message)
+            _logger.warning(message)
             return
 
         if self.callback:
-            records = self.env[self.model].browse(record_ids)
+            records = self.env[self.source_model].browse(record_ids)
             callback = getattr(records, self.callback, None)
             # check if function is exist inside model
             if not callback:
                 message = 'model {} dont have function {}'.format(self.source_model, self.callback)
-                _logger.erro(message)
+                _logger.warning(message)
                 return
-            callback(record_ids)
+            callback()
